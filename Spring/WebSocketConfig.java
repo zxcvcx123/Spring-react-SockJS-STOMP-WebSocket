3b44930e77e1aa0d9db548262d7f3e1886be641a
JungJungMo <128358644+zxcvcx123@users.noreply.github.com>
2023-12-01 16:39:04 +0900

    Update and rename WebSocketConfig.java to Spring/WebSocketConfig.java

--- Spring/WebSocketConfig.java
+++ Spring/WebSocketConfig.java
@@ -1,27 +1,34 @@
 package com.example.pj2be.config.websocket;
 
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
-@RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final WebSocketHandler webSocketHandler;
-
+    // 웹소켓 통신경로를 설정
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic"); // 발행자(요청자)가 해당경로로 메시지를 주면 가공없이 바로 구독자(다른 클라이언트)들에게 전달
+        registry.setApplicationDestinationPrefixes("/app"); // 발행자(요청자)가 해당 경로로 메시지를 주면 구독자(다른 클라이언트)들에게 전달
+                                                            // 참고로 /app/.. 경로로 메시지를 보내면 이 메시지는 @MessageMapping 어노테이션이 붙은 곳으로 향한다.
+    }
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/main")
-                .setAllowedOrigins("*");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gs-guide-websocket")  // 커넥션을 맺는 경로 설정.
+                .withSockJS(); // 프론트에서 SockJS 같이 사용하는 경우
+
+
     }
 }
-    /*
+
+/*
 
         기본적인 웹소켓 흐름: 사용자가 로그인 하면 세션정보를 받고 웹소켓을 연결만 시켜두고,
         실시간 통신(웹소켓 통신)이 필요한 시점에 이벤트를 발생시켜 서버가 일하게 하고,
@@ -39,4 +46,3 @@ public class WebSocketConfig implements WebSocketConfigurer {
                             https (프로토콜), 홈페이지주소(호스트), com(포트)
 
      */
-
