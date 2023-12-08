7ecc5e94e59d57902ce5e4e46dbc314c7c09978d
JungJungMo <128358644+zxcvcx123@users.noreply.github.com>
2023-12-08 11:29:49 +0900

    Update WebSocketController.java

--- Spring/WebSocketController.java
+++ Spring/WebSocketController.java
@@ -6,6 +6,7 @@ import com.example.pj2be.domain.socket.ChatDTO;
 import com.example.pj2be.domain.socket.Greeting;
 import com.example.pj2be.domain.socket.HelloMessage;
 import com.example.pj2be.service.likeservice.BoardLikeService;
+import com.example.pj2be.service.websocketservice.WebSocketService;
 import com.example.pj2be.service.websocketservice.testLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -25,18 +26,10 @@ import java.util.Map;
 @Controller
 @RequiredArgsConstructor
 public class WebSocketController {
-    private final BoardLikeService boardLikeService;
 
-//    @MessageMapping("/hello")
-//    @SendTo("/topic/greetings")
-//    public ChatDTO greeting(ChatDTO chatDTO) throws Exception {
-//        System.out.println("소켓테스트확인됨");
-//        System.out.println(chatDTO);
-//        System.out.println("아이디: " + chatDTO.getId());
-//        System.out.println("내용: " + chatDTO.getChat());
-//
-//        return chatDTO;
-//    }
+    private final WebSocketService webSocketService;
+    private final BoardLikeService boardLikeService;
+    private final SimpMessagingTemplate simpMessagingTemplate; // 특정 유저에게 보내야해서 사용
 
     /* ========== 채팅 ========== */
     @MessageMapping("/hello")
@@ -83,15 +76,15 @@ public class WebSocketController {
     /* ========== 알람 ========== */
     // 댓글알람
     @MessageMapping("/comment/sendalarm/{userId}")
-    @SendTo("/queue/comment/alarm/{toId}")
-    public String addCommentAlarm(@DestinationVariable String userId,
-                                          @RequestBody AlarmDTO alarmDTO,
-                                  @DestinationVariable String toId
-    ) {
+    public void addCommentAlarm(@DestinationVariable String userId,
+                                          @RequestBody AlarmDTO alarmDTO) {
         System.out.println("댓글 알림 userId: " + userId);
         System.out.println("댓글 내용 alarmDTO:" + alarmDTO);
-        toId = "테스트 계정1";
-        return "완룡";
+        String toId = "abcd";
+        Map<String, String> map = new HashMap<>();
+        map.put("인사","안녕하세요");
+        simpMessagingTemplate.convertAndSend("/queue/comment/alarm/" + toId, map);
+
     }
 
 }
