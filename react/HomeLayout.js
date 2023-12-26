c5f049131cac94671bd132e26afefdd6ab3c133a
JungJungMo <128358644+zxcvcx123@users.noreply.github.com>
2023-12-26 11:45:38 +0900

    Update HomeLayout.js

--- react/HomeLayout.js
+++ react/HomeLayout.js
@@ -1,25 +1,29 @@
 import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
-import { Footer } from "./Footer";
 import { Nav } from "./Nav";
 import LoginProvider from "../component/LoginProvider";
-import { createContext, useRef, useState } from "react";
 import Socket from "../socket/Socket";
 
 export function HomeLayout() {
   let test = "테스트1";
+
   return (
-    <LoginProvider>
-      <Box>
-        {/* 모든 영역에서 연결유지 Socket */}
-        <Socket>
-          <Nav />
-          <Outlet context={{ test }} />
-        </Socket>
-        {/* Footer를 바닥에 고정시키려고 빈 컨텐츠 넣었습니다 */}
-        <Box height={"auto"} minHeight="100%" paddingBottom="1000px"></Box>
-        <Footer height="400px" />
-      </Box>
-    </LoginProvider>
+    // <Box bg="blackAlpha.200">
+    <Box bg="rgb(242,242,242)">
+      <LoginProvider>
+        <Box>
+          {/* 모든 영역에서 socket 연결 */}
+          <Socket>
+            <Nav />
+            <Box w={"100%"} m={"0 auto"}>
+              <Outlet context={{ test }} />
+            </Box>
+          </Socket>
+          {/* Footer를 바닥에 고정시키려고 빈 컨텐츠 넣었습니다 */}
+          {/*<Box height={"auto"} minHeight="100%" paddingBottom="1px"></Box>*/}
+          {/*<Footer height="400px" />*/}
+        </Box>
+      </LoginProvider>
+    </Box>
   );
 }
